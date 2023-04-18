6912148bd615e6c9ceb2b58d496d1b3f833e5ae2
SS-victoria <stiven-cool@hotmail.com>
2023-04-17 20:10:37 -0500

    actualizado

---
+++ js/scrippt.js
@@ -0,0 +1,70 @@
+function FichaTecnica(){
+    let logo, nombreproducto, lineaproducto, versionesanteriores, versionactual, modulo, descripciongenpro, descripcionarqui, serhardware, sersoftware, serotros, clientehardware, clientesoftware, clienteotros, requefunciogenera, requeriadicio, version, naturcambio, fechaaprob, fechavalida, version2, naturcambio2, fechaaprob2, fechavalida2, firmaelaboro, firmaaprobo, firmavalido, fechaelaboro, fechaapro, fechavalido
+
+    /*logo=document.getElementById("logo").value
+    document.getElementById("respuesta").innerHTML="<h3>logo: "+logo+"</h3>" */
+
+    nombreproducto=document.getElementById("nombreproducto").value
+    lineaproducto=document.getElementById("lineaproducto").value
+    versionesanteriores=document.getElementById("versionesanteriores").value
+    versionactual=document.getElementById("versionactual").value
+    modulo=document.getElementById("modulo").value
+    descripciongenpro=document.getElementById("descripciongenpro").value
+    descripcionarqui=document.getElementById("descripcionarqui").value 
+    serhardware=document.getElementById("serhardware").value
+    sersoftware=document.getElementById("sersoftware").value
+    serotros=document.getElementById("serotros").value
+    clientehardware=document.getElementById("clientehardware").value
+    clientesoftware=document.getElementById("clientesoftware").value
+    clienteotros=document.getElementById("clienteotros").value
+    requefunciogenera=document.getElementById("requefunciogenera").value
+    requeriadicio=document.getElementById("requeriadicio").value
+    clientproduc=document.getElementById("clientproduc").value
+    version=document.getElementById("version").value
+    naturcambio=document.getElementById("naturcambio").value
+    fechaaprob=document.getElementById("fechaaprob").value
+    fechavalida=document.getElementById("fechavalida").value
+    version2=document.getElementById("version2").value
+    naturcambio2=document.getElementById("naturcambio2").value
+    fechaaprob2=document.getElementById("fechaaprob2").value
+    fechavalida2=document.getElementById("fechavalida2").value
+    firmaelaboro=document.getElementById("firmaelaboro").value
+    firmaaprobo=document.getElementById("firmaaprobo").value
+    firmavalido=document.getElementById("firmavalido").value
+    fechaelaboro=document.getElementById("fechaelaboro").value
+    fechaapro=document.getElementById("fechaapro").value
+    fechavalido=document.getElementById("fechavalido").value
+    
+
+    document.getElementById("respuesta").innerHTML="<h5> "+nombreproducto+"</h5>"
+    document.getElementById("respuesta2").innerHTML="<h5> "+lineaproducto+"</h5>"
+    document.getElementById("respuesta3").innerHTML="<h5> "+versionesanteriores+"</h2>"
+    document.getElementById("respuesta4").innerHTML="<h5> "+versionactual+"</h5>"
+    document.getElementById("respuesta5").innerHTML="<h5> "+modulo+"</h5>"
+    document.getElementById("respuesta6").innerHTML="<h5> "+descripciongenpro+"</h5>"
+    document.getElementById("respuesta7").innerHTML="<h5> "+descripcionarqui+"</h5>"
+    document.getElementById("respuesta8").innerHTML="<h5> "+serhardware+"</h5>"
+    document.getElementById("respuesta9").innerHTML="<h5> "+sersoftware+"</h5>"
+    document.getElementById("respuesta10").innerHTML="<h5> "+serotros+"</h5>"
+    document.getElementById("respuesta11").innerHTML="<h5>"+clientehardware+"</h5>"
+    document.getElementById("respuesta12").innerHTML="<h5>"+clientesoftware+"</h5>"
+    document.getElementById("respuesta13").innerHTML="<h5>"+clienteotros+"</h5>"
+    document.getElementById("respuesta14").innerHTML="<h5>"+requefunciogenera+"</h5>"
+    document.getElementById("respuesta15").innerHTML="<h5>"+requeriadicio+"</h5>"
+    document.getElementById("respuesta16").innerHTML="<h5>"+clientproduc+"</h5>"
+    document.getElementById("respuesta17").innerHTML="<h5>"+version+"</h5>"
+    document.getElementById("respuesta18").innerHTML="<h5>"+naturcambio+"</h5>"
+    document.getElementById("respuesta19").innerHTML="<h5>"+fechaaprob+"</h5>"
+    document.getElementById("respuesta20").innerHTML="<h5>"+fechavalida+"</h5>"
+    document.getElementById("respuesta21").innerHTML="<h5>"+version2+"</h5>"
+    document.getElementById("respuesta22").innerHTML="<h5>"+naturcambio2+"</h5>"
+    document.getElementById("respuesta23").innerHTML="<h5>"+fechaaprob2+"</h5>"
+    document.getElementById("respuesta24").innerHTML="<h5>"+fechavalida2+"</h5>"
+    document.getElementById("respuesta25").innerHTML="<h5>"+firmaelaboro+"</h5>"
+    document.getElementById("respuesta26").innerHTML="<h5>"+firmaaprobo+"</h5>"
+    document.getElementById("respuesta27").innerHTML="<h5>"+firmavalido+"</h5>"
+    document.getElementById("respuesta28").innerHTML="<h5>"+fechaelaboro+"</h5>"
+    document.getElementById("respuesta29").innerHTML="<h5>"+fechaapro+"</h5>"
+    document.getElementById("respuesta30").innerHTML="<h5>"+fechavalido+"</h5>"
+
+}
